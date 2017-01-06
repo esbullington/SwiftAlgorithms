31952892289d58b7811e6610f442dae84fb616bf
Eric S. Bullington <eric.s.bullington@gmail.com>
2017-01-05 20:13:08 -0500

    Add attribution to LinkedList implementation

--- Sources/LinkedList.swift
+++ Sources/LinkedList.swift
@@ -1,3 +1,6 @@
+// Following tutorial at:
+// https://airspeedvelocity.net/2015/07/26/linked-lists-enums-value-types-and-identity/
+// with additions and updates for Swift 3
 enum List<Element> {
 	case End
 	indirect case Node(Element, next: List<Element>)
